6f4f86d48690abdc7dd28869ee855ff4b108482f
Paul Steeles <paulwork@spiff.lol>
2025-04-28 16:50:03 +0100

    first version

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.API_KEY);
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+async function getImage(query) {
+  let response = await fetch(`${endpoint}&q=${query}`);
+  let dataJson = await response.json();
+  let actualUrl = dataJson.data[0].url;
+  console.log(actualUrl);
+}
+
+getImage("monkey");
